7cbd9ddd970ad108bed05156b1d39d1546bf635e
skorpach <skorpach@gmail.com>
2017-10-17 11:58:39 -0700

    Began implementing messaging classes. WIP.

--- dispatch.js
+++ dispatch.js
@@ -7,7 +7,77 @@ var svinfo = {
     name: 'senor pointy'
 };
 
-exports.main = function(res, url_string){
+////////////////////////////////////////////////////////////
+//	Location Class
+////////////////////////////////////////////////////////////
+
+var Location = function(){
+    this.coordinates = null;
+    this.address = null;
+
+    this.setCoords = function(lat, lng, elv){
+	this.coordinates = [lat, lng, elv];
+    };
+    this.setFromParkingSpot = function(ps){
+	if('coordinates' in ps){
+	    this.setCoords(ps.coordinates[0],
+			   ps.coordinates[1],
+			   ps.coordinates[2]);
+	    return true;
+	}
+	return false;
+};
+
+
+
+////////////////////////////////////////////////////////////
+//	Messaging Classes
+////////////////////////////////////////////////////////////
+
+//Top-Level Message class
+var Message = function(){
+    this.sender = "";
+    this.time_sent = null;
+    this.type = "Message";
+
+    //Serialize this object
+    this.serialize = function(){
+	return JSON.stringify(this);
+    };
+};
+
+
+//General request class
+var Request = function(req){
+    Message.call();
+    
+};
+Request.prototype = new Message();
+Request.prototype.constructor = Request;
+
+
+//Parking spot request class
+var SpotRequest = function(req){
+    Request.call(req);
+    
+};
+SpotRequest.prototype = new Request();
+SpotRequest.prototype.constructor = SpotRequest;
+
+
+//Geenral Response class
+var Response = function(req){
+    Message.call();
+    
+};
+Response.prototype = new Message();
+Response.prototype.constructor = Response;
+
+////////////////////////////////////////////////////////////
+//	Main Dispatch Routine
+////////////////////////////////////////////////////////////
+
+exports.main = function(req, res, url_string){
     
     var u = url.parse(url_string, true);
     var ps = routing.getParkingSpots(function(o){
--- index.js
+++ index.js
@@ -9,22 +9,22 @@ app.set('port', (process.env.PORT || 5000));
 
 // Define the home page route.
 app.get('/', function(req, res) {
-  res.send('Our first route is working.:)');
+    res.send('Our first route is working.:)');
 });
 
 app.get('/test', function(req, res) {
-	res.send(
-		{name: ["peter", "lois", "bryan"]}
+    res.send(
+	{name: ["peter", "lois", "bryan"]}
     );
 })
 
 app.get('/parkingspots', function(req, res){
-	dispatch.main(res, "");
+    dispatch.main(req, res, "");
 })
 
 // START THE SERVER
 // ===============================================
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
