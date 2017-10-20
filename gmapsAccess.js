a2d7e13b66d20dcf1602cb75299f8adb80ada5da
skorpach <skorpach@gmail.com>
2017-10-19 17:24:29 -0700

    Added a test file and multi-destination query class. WIP

--- gmapsAccess.js
+++ gmapsAccess.js
@@ -3,7 +3,7 @@ var gMapsClient = require('@google/maps').createClient({
     Promise: Promise
 });
 			  
-var test = function(){
+var test = exports.test = function(){
     gMapsClient.distanceMatrix({origins: ['Washington, DC'],
 				destinations: ['New York, NY'],
 				units: 'metric'}).asPromise()
@@ -14,5 +14,29 @@ var test = function(){
 	    console.log(err);
 	});
 };
+
+
+////////////////////////////////////////////////////////////
+//	Path Ranks Class
+////////////////////////////////////////////////////////////
+//Param origin:		Location object for the origin
+//Param destinations:	(optional) A list of location objects that are the destinations
+var PathRank = exports.PathRank = function(origin, dests){
+    this.origin = origin;
+    this.dest = dest instanceof array ? dests : [];
+    this.ranked = null;
+
+    this.addDest = function(dest){
+	this.dests.append(dest); // TODO: Prevent attaching duplicates
+    };
+
+    this.getRanked() = function(){
+	// TODO: Implement
+    };
+
+    this.query = function(){
+	// TODO: query googleMaps API and return promise.
+    }
+}
 
 test();
---
+++ test/gmapAccessTest.js
@@ -0,0 +1,5 @@
+describe('Google maps destination matrix', function(){
+    it('should pass this test', function(){
+	require('../gmapsAccess.js').test();
+    });
+});
